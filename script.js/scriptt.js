f74af429867b4ca1e03b947ef234311f9fa265d0
Andreas Toullon <andreasgitinit@gmail.com>
2024-12-22 21:43:46 -0300

    testando para ver se o script estava funcionando

---
+++ script.js/scriptt.js
@@ -0,0 +1,22 @@
+const items = [
+    {
+        id:0,
+        nome:'curso',
+        img: 'image.jpg',
+        quantidade:0 
+    },
+    {
+        id:2,
+        nome:'curso',
+        img: 'image.jpg',
+        quantidade:0 
+    },
+    {
+        id:3,
+        nome:'curso',
+        img: 'image.jpg',
+        quantidade:0 
+    },
+]
+
+window.alert("hello world");
